1a003b98d53280895e8e75dfb431f44759444a71
Nobuyoshi Shimmen <nobuyoshi.shimmen@gmail.com>
2018-10-31 22:40:57 +0900

    nonce実装されてた！

--- ext/src/background.js
+++ ext/src/background.js
@@ -1,10 +1,6 @@
 
 chrome.runtime.onMessage.addListener(function (request) {
 
-    function generateRandomString() {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    }
-
     function sendActivateMessage(token) {
         chrome.tabs.query({
             active: true,
@@ -21,23 +17,14 @@ chrome.runtime.onMessage.addListener(function (request) {
 
     if (request.type === 'authenticate') {
 
-        // scope
-        //  - openid if you want an ID Token returned
-        //  - offline_access if you want a Refresh Token returned
-        // device
-        //  - required if requesting the offline_access scope.
-
-        const nonce = generateRandomString();
         let options = {
             audience: 'http://docker.omotenashi.today',
             response_type: 'id_token token',
-            nonce,
         };
 
         new Auth0Chrome(env.AUTH0_DOMAIN, env.AUTH0_CLIENT_ID)
             .authenticate(options)
             .then(function (authResult) {
-                console.log(authResult);
                 localStorage.authResult = JSON.stringify(authResult);
                 chrome.notifications.create({
                     type: 'basic',
